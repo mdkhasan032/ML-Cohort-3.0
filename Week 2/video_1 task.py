6a8470b406b491ca379a04e78f1f036efd80c9dc
Md. Kamrul Hasan <khshakil0000@gmail.com>
2025-01-26 16:33:22 +0600

    Add files via upload

---
+++ Week 2/video_1 task.py
@@ -0,0 +1,15 @@
+#using class and object
+class rectangle ():
+    length = ""
+    width = ""
+a = rectangle
+a.length = 10
+a.width = 5
+area = a.length * a.width
+print (f"Area of rectangle =",area)
+
+
+
+
+
+       
